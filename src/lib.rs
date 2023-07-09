5fb17c0c579e9d0b2185d23495e4fb894ed75368
nuno <41233686+qope@users.noreply.github.com>
2023-07-09 20:45:20 +0900

    add starky proof

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ pub mod prover_native;
 pub mod statements;
 pub mod transcript_circuit;
 pub mod transcript_native;
+pub mod starky_prover;
 pub mod verifier_circuit;
 pub mod verifier_native;
 pub mod verifier_witness;
